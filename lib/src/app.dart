7bc306c8707f49ac802876fa4e3505908fa0b246
fidoguido <guido.alvarez.martinez@gmail.com>
2021-02-24 18:47:19 -0500

    Mi primer Commit

---
+++ lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Componentes App',
+        home: HomePage(),
+      );
+}
